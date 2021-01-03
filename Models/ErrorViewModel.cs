848969e1e5a662aef93dd318b2e89ca319c85010
Nata23 <natanaeltic2005@hotmail.com>
2021-01-03 14:52:12 +0100

    IdentityMensagemPt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Alteracao_IdentityNetcore5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
